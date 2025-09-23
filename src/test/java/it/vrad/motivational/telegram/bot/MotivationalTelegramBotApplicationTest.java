4ce4ba860e01772a2080b183a134567c24666359
userAdrian <144135193+userAdrian@users.noreply.github.com>
2025-09-23 18:28:05 +0200

    Changed suffix from *Tests to Test

---
+++ src/test/java/it/vrad/motivational/telegram/bot/MotivationalTelegramBotApplicationTest.java
@@ -0,0 +1,12 @@
+package it.vrad.motivational.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest FIXME: add docker to mock a redis server with testcontainers
+class MotivationalTelegramBotApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
