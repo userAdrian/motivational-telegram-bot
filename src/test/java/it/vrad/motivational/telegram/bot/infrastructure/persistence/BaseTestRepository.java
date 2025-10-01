cf5f8fcad613f13a1487aff810d4a6f17764929a
userAdrian <144135193+userAdrian@users.noreply.github.com>
2025-10-01 15:21:24 +0200

    Moved assertRecursiveEqualsIgnoringId method to TestAssertions

--- src/test/java/it/vrad/motivational/telegram/bot/infrastructure/persistence/BaseTestRepository.java
+++ src/test/java/it/vrad/motivational/telegram/bot/infrastructure/persistence/BaseTestRepository.java
@@ -1,22 +1,12 @@
 package it.vrad.motivational.telegram.bot.infrastructure.persistence;
 
-import it.vrad.motivational.telegram.bot.infrastructure.persistence.constants.PersistenceTestConstants;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @EntityScan(basePackages = "it.vrad.motivational.telegram.bot.infrastructure.persistence.entity")
 @EnableJpaRepositories(basePackages = "it.vrad.motivational.telegram.bot.infrastructure.persistence.repository")
 public abstract class BaseTestRepository {
 
-    protected static <T> void assertRecursiveEqualsIgnoringId(T actual, T expected) {
-        assertThat(actual)
-                .usingRecursiveComparison()
-                .ignoringFieldsMatchingRegexes(PersistenceTestConstants.ID_FIELD_NAME_REGEX)
-                .isEqualTo(expected);
-    }
 }
