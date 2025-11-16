4695cc0b0b4b384de0f2d4d31af6ae9129b184bc
userAdrian <144135193+userAdrian@users.noreply.github.com>
2025-11-16 18:12:54 +0100

    Add @SpringBootTest to verify application context loads correctly

--- src/test/java/it/vrad/motivational/telegram/bot/MotivationalTelegramBotApplicationTest.java
+++ src/test/java/it/vrad/motivational/telegram/bot/MotivationalTelegramBotApplicationTest.java
@@ -1,12 +1,15 @@
 package it.vrad.motivational.telegram.bot;
 
+import it.vrad.motivational.telegram.bot.infrastructure.cache.EnableRedisContainer;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest FIXME: add docker to mock a redis server with testcontainers
+@SpringBootTest
+@EnableRedisContainer
 class MotivationalTelegramBotApplicationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
